5887e97bd21f975eb5185571f5f741b2e61bffe6
Réka Aranyosi <reka.aranyosi@stud.uni-corvinus.hu>
2023-11-28 08:28:25 +0100

    múltheti folytatása

---
+++ JokesApplicationSolution/wwwroot/scripts/geek.js
@@ -0,0 +1,12 @@
+﻿var viccek;
+
+fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+}

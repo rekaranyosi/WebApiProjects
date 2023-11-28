7705dba61d0baa67cd654579163486b1a1788e74
Réka Aranyosi <reka.aranyosi@stud.uni-corvinus.hu>
2023-11-28 09:04:11 +0100

    11 gyak

---
+++ JokesApplicationSolution/Controllers/TesztController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesApplicationSolution.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+            //vagy rövidebben:
+
+            //string pontosIdő = DateTime.Now.ToShortTimeString();
+            //return Ok(pontosIdő);
+        }
+
+
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = szoveg.ToUpper()
+            };
+
+            //Vagy egyszerűbben:
+            //return Ok(szoveg.ToUpper());
+
+            //return BadRequest("Nem jó a bemenő adat");
+        }
+    }
+}
--- JokesApplicationSolution/wwwroot/scripts/geek.js
+++ JokesApplicationSolution/wwwroot/scripts/geek.js
@@ -1,10 +1,19 @@
 ﻿var viccek;
 
+windows.onload(){
+    Letöltés();
+}
+
 fetch('/jokes.json')
     .then(response => response.json())
     .then(data => letöltésBefejeződött(data)
     );
 
+function Letöltés() {
+    for (var i = 0; i < length; i++) {
+
+    }
+}
 function letöltésBefejeződött(d) {
     console.log("Sikeres letöltés")
     console.log(d)

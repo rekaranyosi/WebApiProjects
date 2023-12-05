02d84d2a53223e95456c597279751396d9d83f86
Réka Aranyosi <reka.aranyosi@stud.uni-corvinus.hu>
2023-12-05 08:29:58 +0100

    hajos_12gyak

---
+++ 12_gyak_hajosExcel/Form1.cs
@@ -0,0 +1,106 @@
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace _12_gyak_hajosExcel
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következõ feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
+
+        }
+
+        void CreateTable()
+        {
+            string[] fejlécek = new string[] {
+        "Kérdés",
+        "1. válasz",
+        "2. válaszl",
+        "3. válasz",
+        "Helyes válasz",
+        "kép"};
+
+            for (int i = 0; i < fejlécek.Length+1; i++)
+            {
+                xlSheet.Cells[1, 1] = fejlécek[0];
+            }
+
+            Models.HajosContext hajosContext = new Models.HajosContext();
+            var mindenKérdés = hajosContext.Questions.ToList();
+
+            object[,] adatTömb = new object[mindenKérdés.Count(), fejlécek.Count()];
+
+            for (int i = 0; i < mindenKérdés.Count(); i++)
+            {
+                adatTömb[i, 0] = mindenKérdés[i].Question1;
+                adatTömb[i, 1] = mindenKérdés[i].Answer1;
+                adatTömb[i, 2] = mindenKérdés[i].Answer2;
+                adatTömb[i, 3] = mindenKérdés[i].Answer3;
+                adatTömb[i, 4] = mindenKérdés[i].CorrectAnswer;
+                adatTömb[i, 5] = mindenKérdés[i].Image;
+            }
+
+            int sorokSzáma = adatTömb.GetLength(0);
+            int oszlopokSzáma = adatTömb.GetLength(1);
+
+            Excel.Range adatRange = xlSheet.get_Range("A2", Type.Missing).get_Resize(sorokSzáma, oszlopokSzáma);
+            adatRange.Value2 = adatTömb;
+            adatRange.Columns.AutoFit();
+
+            FormatTable();
+        }
+
+        private void FormatTable()
+        {
+            Excel.Range fejllécRange = xlSheet.get_Range("A1", Type.Missing).get_Resize(1, 6);
+            fejllécRange.Font.Bold = true;
+            fejllécRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            fejllécRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            fejllécRange.EntireColumn.AutoFit();
+            fejllécRange.RowHeight = 40;
+            fejllécRange.Interior.Color = Color.Fuchsia;
+            fejllécRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+        }
+    }
+}
